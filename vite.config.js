b7c3cc744c93685a9f2c5bd8751f9f455be78bb1
Denis <dnsmarinov23@gmail.com>
2025-11-24 22:48:23 +0200

    fix tailwind config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     server: {
@@ -14,8 +15,9 @@ export default defineConfig({
     },
     plugins: [
         laravel({
-            input: ['resources/css/app.scss', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        tailwindcss(),
     ],
 });
